5f8066957541a763649d736016d206cfb2cc74dc
HenriqueDeAssuncao <104686167+HenriqueDeAssuncao@users.noreply.github.com>
2023-05-02 11:40:20 -0300

    Mudei o header

--- templates/header.php
+++ templates/header.php
@@ -16,36 +16,36 @@
 </head>
 <body>
 
-<header class="Container">
-    <nav id="nav" class="Flex">
-        <div id="nav-icons">
-            <button id="btn-hamburguer">
-                <i class="fa-solid fa-bars"></i>
-            </button>
-            <button id="btn-close">
-                <i class="fa-solid fa-xmark"></i>
-            </button>
-        </div>
-        <div id="nav-1">
-            <a href="<?=$CURRENT_URL?>/" id="logo"><img src="" alt="Hfashion"></a>
-            <ul id="links-nav-1">
-                <li class="links"><a href="<?=$CURRENT_URL?>/dashboard.php">Dashboard</a></li>
-                <li class="links"><a href="<?=$CURRENT_URL?>/quizzes.php">Quizzes</a></li>
-                <li class="links"><a href="<?=$CURRENT_URL?>/ranking.php">Classificação</a></li>
-                <!-- A página adm.php é restrita, então vai ter que ter tratamento do backend depois -->
-                <li class="links"><a href="<?=$CURRENT_URL?>/adm.php">ADM</a></li> 
-            </ul>
-        </div>
-
-        <img src="" alt="Hfashion" class="hidden">
-        
-        <div id="nav-2">
-            <!-- Aqui eu vou verificar se o usuário está logado. Se ele estiver, eu mostro pra ele esses botões, senão, eu mostro a foto de perfil dele que redireciona para dashboard. Mas no mobile é só a foto de perfil msm -->
-            <input type="text"> 
-            <ul id="links-nav-2">
-                <li class="links"><a href="<?=$CURRENT_URL?>/sign_in.php">Conectar</a></li>
-                <li class="red"><a href="<?=$CURRENT_URL?>/sign_up.php">Criar Conta</a></li>
-            </ul>
-        </div>
-    </nav>
+<header id="header">
+    <div class="Container">
+        <nav id="nav" class="Flex">
+            <div id="nav-icons">
+                <button id="btn-hamburguer">
+                    <i class="fa-solid fa-bars"></i>
+                </button>
+                <button id="btn-close" class="Hidden">
+                    <i class="fa-solid fa-xmark"></i>
+                </button>
+            </div>
+            <div id="nav-1">
+                <a href="<?=$CURRENT_URL?>/" id="logo" class="Hidden"><img src="" alt="Hfashion"></a>
+                <ul id="links-nav-1">
+                    <li class="links"><a href="<?=$CURRENT_URL?>/dashboard.php">Dashboard</a></li>
+                    <li class="links"><a href="<?=$CURRENT_URL?>/edit_profile.php">Editar Perfil</a></li>
+                    <li class="links"><a href="<?=$CURRENT_URL?>/quizzes.php">Quizzes</a></li>
+                    <li class="links"><a href="<?=$CURRENT_URL?>/ranking.php">Classificação</a></li>
+                    <!-- A página adm.php é restrita, então vai ter que ter tratamento do backend depois -->
+                    <li class="links"><a href="<?=$CURRENT_URL?>/adm.php">ADM</a></li> 
+                </ul>
+            </div>
+            <img src="" alt="Hfashion" id="logo-mobile">
+            <div id="nav-2">
+                <a href="">
+                    <div class="profile-img">
+                        <img src="<?=$CURRENT_URL?>/dashboard.php>" alt="Foto de Perfil">
+                    </div>
+                </a>
+            </div>
+        </nav>
+    </div>
 </header>
