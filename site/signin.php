cd5cb57c84cc2ae89abc36bdcc63bbbac890e1f2
Henrique <104686167+HenriqueDeAssuncao@users.noreply.github.com>
2023-05-05 12:11:33 -0300

    php do signin

--- site/signin.php
+++ site/signin.php
@@ -6,7 +6,7 @@
     <form action="<?=$CURRENT_URL?>/auth_process.php" method="POST">
         <input type="hidden" name="type" value="login">
         <div class="form-group">
-            <label for="nickname_email">E-mail ou Apelido:</label>
+            <label class="email" for="nickname_email">E-mail ou Apelido:</label>
             <input type="text" class="form-input" id="nickname_email" name="nickname_email">
         </div>
         <div class="form-group">
@@ -14,7 +14,7 @@
             <input type="password" class="form-input" id="password" name="password">
         </div>
         <div class="form-container">
-            <input type="submit" class="Button" value="Conectar">
+            <input type="submit" class="Button btn-con" value="Conectar">
             <div class="form-group">
                 <p>Não tem uma conta?</p>
                 <p><a href="<?=$CURRENT_URL?>/signup.php">Faça seu cadastro</a></p>
