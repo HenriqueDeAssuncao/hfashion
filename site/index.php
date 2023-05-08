1c3e154ea2a134da50aecc7d87404ce0c3a43f45
HenriqueDeAssuncao <104686167+HenriqueDeAssuncao@users.noreply.github.com>
2023-05-08 14:23:18 -0300

    Index

--- site/index.php
+++ site/index.php
@@ -3,8 +3,8 @@
 ?>
   </div>
     <div id="container-banner">
-     <img src="img/banner.png" alt="Principais personagens de High School" id="img-banner">   
-    </div>
+    <img src="img/banner.png" alt="Principais personagens de High School" id="img-banner">   
+  </div>
     <div id="container-timeline">
       <p class="style_maintext"> 
       Como surgiu a moda?
@@ -12,9 +12,10 @@
       <p class="style_mediumtext">
       confira a linha do tempo da moda e os acontecimentos mais marcantes  
       </p>
-      <img src="img/timeline.png" alt="" id="img-timeline">
+      <div class="Container">
+        <img src="img/timeline.png" alt="" id="img-timeline">
+      </div>
     </div>
-
 
 <?php
     require_once("templates/footer.php");
