79eee124b778fb919fa76b915a01020ae65dfccd
jhosl <90850516+jhosl@users.noreply.github.com>
2023-05-19 12:09:34 -0300

    Update index.php
    mudei coisa

--- site/index.php
+++ site/index.php
@@ -1,20 +1,23 @@
 <?php
     require_once("templates/header.php");
 ?>
+
+<!-- FOTO DE FUNDO -->
   </div>
     <div id="container-banner" style="background-image: url(<?=$CURRENT_URL?>/img/index/banner.svg); width:100%; height:940px;">
-  
-  </div>
-    <div id="container-timeline">
-      <p class="style_maintext"> 
-      Como surgiu a moda?
-      </p>
-      <p class="style_mediumtext">
-      confira a linha do tempo da moda e os acontecimentos mais marcantes  
-      </p>
-      <div class="Container">
-        <img src="img/timeline.png" alt="" id="img-timeline">
+    <div id="container-logo"> 
+      <p id="txt_logo_high">CONHEÇA A MODA DE</p>
+      <img src="img/index/logo_high.svg" id="img_logo_high">
+      <div id="btn-roll">
+      <button><a href="#container-aboutus">CONHEÇA O HIFASHION </a></button>
+
       </div>
+    </div> 
+  </div>
+  
+<!-- SOBRE NOS -->
+    <div id="container-aboutus">
+      
     </div>
 
 <?php
