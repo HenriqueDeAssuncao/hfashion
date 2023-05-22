3aad6ea20fbe15690434f3424b7194d711fdf426
Henrique <104686167+HenriqueDeAssuncao@users.noreply.github.com>
2023-05-22 11:46:42 -0300

    Fiz o arquivo pra colocar os quizzes

---
+++ site/data/quizzes.php
@@ -0,0 +1,89 @@
+<?php
+//Quizzes as quiz as id and questions as question
+    $quizzes = 
+    [
+        [
+            'id' => 1,
+            'title' => 'Quiz01',
+            'questions' =>
+                [
+                    [
+                        'name' => '1)pergunta?',
+                        'options' => 
+                        [
+                            '1 - opção 1',
+                            '2 - opção 2',
+                            '3 - opção 3',
+                            '4 - opção 4'
+                        ],
+                        'r-answer' => 1
+            
+                    ],
+                    [
+                        'name' => '2)pergunta?',
+                        'options' => 
+                        [
+                            '1 - opção 1',
+                            '2 - opção 2',
+                            '3 - opção 3',
+                            '4 - opção 4'
+                        ],
+                        'r-answer' => 3
+                    ],
+                    [
+                        'name' => '3)pergunta?',
+                        'options' => 
+                        [
+                            '1 - opção 1',
+                            '2 - opção 2',
+                            '3 - opção 3',
+                            '4 - opção 4'
+                        ],
+                        'r-answer' => 2
+                    ]
+                ]
+        ],
+
+        [
+            'id' => 2,
+            'title' => 'Quiz02',
+            'questions' =>
+                [
+                    [
+                        'name' => '1)pergunta?',
+                        'options' => 
+                        [
+                            '1 - opção 1',
+                            '2 - opção 2',
+                            '3 - opção 3',
+                            '4 - opção 4'
+                        ],
+                        'r-answer' => 1
+            
+                    ],
+                    [
+                        'name' => '2)pergunta?',
+                        'options' => 
+                        [
+                            '1 - opção 1',
+                            '2 - opção 2',
+                            '3 - opção 3',
+                            '4 - opção 4'
+                        ],
+                        'r-answer' => 3
+                    ],
+                    [
+                        'name' => '3)pergunta?',
+                        'options' => 
+                        [
+                            '1 - opção 1',
+                            '2 - opção 2',
+                            '3 - opção 3',
+                            '4 - opção 4'
+                        ],
+                        'r-answer' => 2
+                    ]
+                ]
+        ]
+    ]
+?>
