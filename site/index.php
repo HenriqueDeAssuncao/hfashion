436981484b625cbf1a21fc3ccc8d76de3d49028b
jhosl <90850516+jhosl@users.noreply.github.com>
2023-05-22 12:05:53 -0300

    Update index.php
    FINALIZEI SOBRE NOS

--- site/index.php
+++ site/index.php
@@ -8,19 +8,37 @@
     <div id="container-logo"> 
       <p id="txt_logo_high">CONHEÇA A MODA DE</p>
       <img src="img/index/logo_high.svg" id="img_logo_high">
-      <div id="btn-roll">
-      <button><a href="#container-aboutus">CONHEÇA O HIFASHION </a></button>
-
-      </div>
+        <div id="div-btn-roll">
+      <a href="#container-aboutus" id="btn-roll"> CONHEÇA O HIFASHION </a>
+        </div>
     </div> 
   </div>
   
 <!-- SOBRE NOS -->
     <div id="container-aboutus">
-      
+      <div id= "info-aboutus">
+        <div id="div_img_aboutus">
+          <img src="img/index/logo_circle.svg" id="img_logo_aboutus">
+        </div>
+        <div id="div_text_aboutus">
+          <p class="style-maintext" id="align-aboutus"> De onde surgiu o HiFashion?</p>
+          <p class="style-regulartext">Learning with Duolingo is fun, and research shows that it works! With quick, bite-sized lessons,you’ll earn points and unlock new levels while gaining real-world communication skills.</p>
+        </div>
+      </div>
     </div>
+    <hr class="hr">
+    
+<!-- O QUE VOCE VAI APRENDER -->    
+    <div id="container-oqvcvera">
+      <div id="info-oqvcvera">
+        <div id="div_title">
+          <p class="style-maintext">O que você vai aprender?</p>
+        </div>
+
+      </div>
+    </div>
+    <hr class="hr">
 
 <?php
     require_once("templates/footer.php");
 ?>
-
