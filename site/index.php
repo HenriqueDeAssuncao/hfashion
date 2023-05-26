ad2326338644d3b9306eb82047a7011d637c0d3e
Etec <etec@etec.com>
2023-05-26 11:16:14 -0300

    Update index.php

--- site/index.php
+++ site/index.php
@@ -6,7 +6,7 @@ require_once("templates/header.php");
 <div id="container-banner"
   style="background-image: url(<?= $CURRENT_URL ?>/img/index/banner.svg); width:100%; height:940px;">
   <div id="container-logo">
-    <p id="txt_logo_high">CONHEÇA A MODA DE</p>
+    <p id="txt_logo_high">CONHEÇA O MODO DE</p>
     <img src="img/index/logo_high.svg" id="img_logo_high">
     <div id="div-btn-roll">
       <a href="#container-aboutus" id="btn-roll"> CONHEÇA O HIFASHION </a>
