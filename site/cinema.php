350c7b07fc381c0bd565d1da038a1f60acdfc3bd
Etec <etec@etec.com>
2023-06-06 11:55:48 -0300

    cinema

---
+++ site/cinema.php
@@ -0,0 +1,18 @@
+<?php
+require_once "templates/header.php";
+?>
+
+ 
+    
+    <div class="Container">
+    <div id="container-banner" style="background-image:url(<?= $CURRENT_URL ?>/img/cinema/esporte.png);">
+    </div>
+  
+
+
+
+
+    <link rel="stylesheet" href="<?=$CURRENT_URL?>/css/cinema.css">
+<?php
+require_once "templates/footer.php";
+?>
