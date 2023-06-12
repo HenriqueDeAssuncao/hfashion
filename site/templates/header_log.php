898a739a1bd00997f777a15aa83379a0b26905dd
Etec <etec@etec.com>
2023-06-12 07:33:11 -0300

    consertei o padding

--- site/templates/header_log.php
+++ site/templates/header_log.php
@@ -37,25 +37,11 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body id="body">
-                    <ul id="links-nav-2">
-                        <?php if(!empty($userData)):?>
-                            <li class="links"><a href="<?=$CURRENT_URL?>/edit_profile.php">Editar Perfil</a></li>
-                            <li class="links"><a href="<?=$CURRENT_URL?>/process/logout_process.php">Sair</a></li>
-                        <?php endif;?>
-                        <?php if(empty($userData)):?>
-                        <?php endif;?>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    </div>
-</header>
 
 <main>                          
-    <div class="Container">
-        <?php if(!empty($flashMessage)):?>
-            <div class="msg-<?=$flashMessage["type"];?> Flex">
-				<div id="msgIcon"><i class="<?=$flashMessage["icon"]?>"></i></div>
-                <p><?=$flashMessage["msg"];?></p>
-            </div>
-        <?php endif;?>
+    <?php if(!empty($flashMessage)):?>
+        <div class="msg-<?=$flashMessage["type"];?> Flex">
+            <div id="msgIcon"><i class="<?=$flashMessage["icon"]?>"></i></div>
+            <p><?=$flashMessage["msg"];?></p>
+        </div>
+    <?php endif;?>
