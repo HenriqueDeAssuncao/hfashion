5225ee823b9c46e5f138ef3e1108c6abd579e797
Etec <etec@etec.com>
2023-06-12 11:15:44 -0300

    Update edit_profile.php

--- site/edit_profile.php
+++ site/edit_profile.php
@@ -1,26 +1,26 @@
 <?php
-    require_once("templates/header.php");
-    require_once("models/User.php");
-    require_once("dao/UserDAO.php");
+require_once("templates/header.php");
+require_once("models/User.php");
+require_once("dao/UserDAO.php");
 
-    $user = new User();
-    $userDao = new UserDAO($conn, $CURRENT_URL);
-    $userData = $userDao->verifyToken(true);
+$user = new User();
+$userDao = new UserDAO($conn, $CURRENT_URL);
+$userData = $userDao->verifyToken(true);
 ?>
 
 <div id="edit-container">
-    <form action="<?=$CURRENT_URL?>/process/user_process.php" method="POST" enctype="multipart/form-data">
+    <form action="<?= $CURRENT_URL ?>/process/user_process.php" method="POST" enctype="multipart/form-data">
         <input type="hidden" name="type" value="update">
-        <h1>Altere seus dados no formulário abaixo:</h1>
-		<h2>Perfil:</h2>
+        <h2>Altere seus dados no formulário abaixo jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj:</h2>
+        <h2>Perfil:</h2>
         <div class="form-group">
             <label for="image">Foto:</label>
-            <input type="file" name="image"> 
-	<div class="profile-img" style="width: 100px; height: 100px; background-image: url('<?=$CURRENT_URL?>/img/users/<?=$image?>')" alt="Foto de Perfil"></div>
+            <input type="file" name="image">
+            <div class="profile-img" style="width: 100px; height: 100px; background-image: url('<?= $CURRENT_URL ?>/img/users/<?= $image ?>')" alt="Foto de Perfil"></div>
         </div>
-	<div class="form-group">
+        <div class="form-group">
             <label for="bio">Altere sua bio:</label>
-            <input type="bio" class="form-input" name="bio" value="<?=$userData->getBio()?>">
+            <input type="bio" class="form-input" name="bio" value="<?= $userData->getBio() ?>">
         </div>
         <div class="form-group">
             <label for="nickname">Digite o seu novo apelido:</label>
@@ -31,8 +31,8 @@
         </div>
     </form>
 
-    <form action="<?=$CURRENT_URL?>/process/user_process.php" method="POST">
-    <input type="hidden" name="type" value="changepassword">
+    <form action="<?= $CURRENT_URL ?>/process/user_process.php" method="POST">
+        <input type="hidden" name="type" value="changepassword">
         <h2>Senha:</h2>
         <p>Digite a sua nova senha e confirme para alterá-la:</p>
         <div class="form-group">
@@ -45,11 +45,11 @@
         </div>
         <div class="form-container">
             <input type="submit" class="Button" value="Alterar">
-        </div>
+        <div>
     </form>
 
 </div>
 
 <?php
-    require_once("templates/footer.php");
+require_once("templates/footer.php");
 ?>
