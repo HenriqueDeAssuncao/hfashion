c5b5c940a54c1a48f2a0235b2fb7e5094182c225
HenriqueDeAssuncao <104686167+HenriqueDeAssuncao@users.noreply.github.com>
2023-06-14 18:41:01 -0300

    update header

--- site/script/main.js
+++ site/script/main.js
@@ -1,8 +1,13 @@
-//NAVBAR
+//NAVBAR ELEMENTOS
 const btnHamburguer = document.querySelector('.btn-hamburguer');
 const btnClose = document.querySelector('.btn-close');
 const nav = document.querySelector('.nav-1');
+let verifyScreenWidth = window.matchMedia('(max-width: 992px)');
+//DROPDOWN BAR ELEMENTOS
+const btnDropdown = document.querySelector('.profile-pic-header');
+const dropdownRect = document.querySelector('.dropdown-rect');
 
+//FUNÇÕES SIDE BAR
 function slide() {
     btnHamburguer.style.display = "none";
     btnClose.style.display = "block";
@@ -14,16 +19,10 @@ function hide() {
     btnHamburguer.style.display = "block";
     btnClose.style.display = "none";
     nav.style.left = "-100%";
+    nav.style.backgroundColor = "transparent";
 }
 
-btnHamburguer.addEventListener("click", slide);
-btnClose.addEventListener("click", hide);
-
-
-//DROPDOWN BAR
-const btnDropdown = document.querySelector('.profile-pic-header');
-const dropdownRect = document.querySelector('.dropdown-rect');
-
+//FUNÇÕES ANÔNIMAS DROPDOWN MENU
 btnDropdown.addEventListener("click", () => {
     dropdownRect.classList.toggle('Hidden');
 });
@@ -33,3 +32,47 @@ document.addEventListener("click", ({ target }) => {
         dropdownRect.classList.add("Hidden");
     }
 });
+
+//JS ESPECIAL PARA O INDEX
+if (window.location.href == "http://localhost/hfashion/site/") {
+    //HEADER ELEMENTOS
+    const banner = document.querySelector('.js-container-banner');
+    const header = document.querySelector('.js-header');
+    const logo = document.querySelector('.logo-desktop');
+    const links = document.querySelectorAll('.links-nav-1 a');
+
+    //COLOCO A LOGO BRANCA
+    logo.setAttribute('src', 'img/header/logo.svg')
+
+    function verifyScroll() {
+        if (window.pageYOffset >= banner.clientHeight) {
+            header.style.backgroundColor = 'white';
+            btnHamburguer.classList.remove('Color-white');
+            logo.setAttribute('src', 'img/header/logo-black.svg')
+            links.forEach((link) => {
+                link.style.color = "black";
+            })
+        } else {
+            header.style.backgroundColor = 'transparent';
+            btnHamburguer.classList.add('Color-white');
+            logo.setAttribute('src', 'img/header/logo.svg')
+            links.forEach((link) => {
+                link.style.color = "white";
+            })
+        }
+    }
+
+    window.addEventListener('scroll', verifyScroll);
+}
+
+//ADICIONO OS EVENTOS SEM ALTERAR O MENU
+if (verifyScreenWidth.matches) {
+    btnHamburguer.addEventListener("click", () => {
+        slide();
+    });
+    btnClose.addEventListener("click", () => {
+        hide();
+    });
+}
+
+
