58db55e16cb1e7560174b127741fcf663c2b0ebf
Etec <etec@etec.com>
2023-06-20 11:57:55 -0300

    ;;;

--- site/y2k.php
+++ site/y2k.php
@@ -2,19 +2,29 @@
 require_once "templates/header.php";
 ?>
 
- 
-    
  <div class="container-banner">
+
         <div id="div-img-banner">
             <img  src="img/y2k/y2k.svg"  id="img-banner">
         </div>  
-    <div id="titutlo-aboutus">
-        <div id="titulo-y2k">
-        Y2K
-        </div>
+
+    <div id="titulo">
+        <p id="titulo-y2k">Y2K</p>
     </div>
+
  </div>
 
+ <div class="container-primeiro-pag">
+  <div id="div-primeiro-pag">
+    <p id="primeiro-pag">A estética Y2K é uma tendência que remonta ao final dos anos 90 e ao início dos anos 2000. Y2K é uma abreviação de "Year 2000",
+      que se refere à época em que muitas pessoas temiam que o mundo sofresse um colapso devido a um suposto erro do "bug do milênio".
+      A estética Y2K é uma espécie de homenagem nostálgica a essa era.</p>
+  </div>
+
+  <div id="img-peimeiro-pag">
+    <img src="img/y2k/y2k_artigo.png" id="img-pag">
+  </div>
+</div>
 
 
 
