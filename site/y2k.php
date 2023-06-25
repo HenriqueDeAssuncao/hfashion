7530aae530f69ed13df5b047ff8a73879f59e7d9
jesbikers <jessica.almeida171@etec.sp.gov.br>
2023-06-24 23:14:04 -0300

    ...

--- site/y2k.php
+++ site/y2k.php
@@ -19,6 +19,10 @@ require_once "templates/header.php";
     <p id="primeiro-pag">A estética Y2K é uma tendência que remonta ao final dos anos 90 e ao início dos anos 2000. Y2K é uma abreviação de "Year 2000",
       que se refere à época em que muitas pessoas temiam que o mundo sofresse um colapso devido a um suposto erro do "bug do milênio".
       A estética Y2K é uma espécie de homenagem nostálgica a essa era.</p>
+
+      <p id="segundo-pag">Essa estética é caracterizada por elementos futuristas, cyberpunk e kitsch, muitas vezes com uma mistura de tecnologia 
+        antiga e moderna. As cores costumam ser brilhantes e vibrantes, e a moda Y2K inclui roupas justas, tecidos brilhantes, calças de cintura 
+        baixa, tops cropped, tênis plataforma e acessórios como correntes e cintos largos.</p>
   </div>
 
   <div id="img-peimeiro-pag">
