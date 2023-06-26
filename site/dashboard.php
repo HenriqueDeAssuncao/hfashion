2da5ab3ef5bf1e852bcebc4a3a300c5cd048af80
erikakaka01 <whickeriano98@hotmail.com>
2023-06-26 19:35:56 -0300

    dashboard
    aaleleklek

--- site/dashboard.php
+++ site/dashboard.php
@@ -1,10 +1,9 @@
+<link rel="stylesheet" type="text/css" href="css/dashboard.css">
 
-    <link rel="stylesheet" type="text/css" href="css/dashboard.css">
-    
 <?php
-    require_once("templates/header.php");
-    require_once("models/User.php");
-    $userData = $userDao->verifyToken(true);
+require_once("templates/header.php");
+require_once("models/User.php");
+$userData = $userDao->verifyToken(true);
 ?>
 
 
@@ -12,34 +11,34 @@
 
 
 <div id="container_profile">
-    <div id="info_profile">
+  <div id="info_profile">
 
-        <div id="div_img_profile">
-            <img src="img/dashboard/cadastro.png" alt="" id="img_profile">
-        </div>
+    <div id="div_img_profile">
+      <img src="img/dashboard/cadastro.png" alt="" id="img_profile">
+    </div>
 
-        <p class="style-maintext"> Ana Carolina </p>
-        <p class="style-regulartext"> anacarolina@gmail.com </p>
-        <a href="http://localhost/hfashion/site/edit_profile.php" id="link_edit_profile">
-            editar perfil
-        </a>
-        
+    <p class="style-maintext"> Ana Carolina </p>
+    <p class="style-regulartext"> anacarolina@gmail.com </p>
+    <a href="http://localhost/hfashion/site/edit_profile.php" id="link_edit_profile">
+      editar perfil
+    </a>
 
-        <div id="div_bio_profile">
-            <p class="style-regulartext"> 
-             A vida é engual uma montanha russa, um dia 
-             a gente estamos emcima e no outro embaixo 
-            </p>
-        </div>
 
-        <div id="align_div_emblemas">
-            <img src="img/dashboard/emblema1.png" class="img_emblema">
-            <img src="img/dashboard/emblema2.png" class="img_emblema">
-            <img src="img/dashboard/emblema3.png" class="img_emblema">
-            <img src="img/dashboard/emblema3.png" class="img_emblema">
-        </div>
+    <div id="div_bio_profile">
+      <p class="style-regulartext">
+        A vida é engual uma montanha russa, um dia
+        a gente estamos emcima e no outro embaixo
+      </p>
+    </div>
 
-<!--
+    <div id="align_div_emblemas">
+      <img src="img/dashboard/emblema1.png" class="img_emblema">
+      <img src="img/dashboard/emblema2.png" class="img_emblema">
+      <img src="img/dashboard/emblema3.png" class="img_emblema">
+      <img src="img/dashboard/emblema3.png" class="img_emblema">
+    </div>
+
+    <!--
         <div id="div_btn-roll">
             <a href="http://localhost/hfashion/site/edit_profile.php" id="btn-roll">
             <img src="img/dashboard/camera.png" id="img_camera">
@@ -47,7 +46,7 @@
             </a>
         </div>
 -->
-    </div>
+  </div>
 </div>
 
 
@@ -57,11 +56,11 @@
 
 
 <div id="container_quiz">
-    <div id="info_quiz">
+  <div id="info_quiz">
 
-       <div id="align_title_quiz">
-            <p class="style-maintext"> Quizzes </p>
-       </div>
+    <div id="align_title_quiz">
+      <p> Quizzes </p>
+    </div>
 
     <div id="div_btn">
 
@@ -79,7 +78,7 @@
         </a>
 
         <a href="" class="btn_quizzes">
-          <img src="img/dashboard/trofeu.png" id="img_relogio_quiz">  
+          <img src="img/dashboard/trofeu.png" id="img_relogio_quiz">
           <p>Y2K</p>
         </a>
 
@@ -90,9 +89,9 @@
 
       </div>
 
-        </div>
-
     </div>
+
+  </div>
 </div>
 
 
@@ -101,57 +100,77 @@
 
 
 <div id="container_conquistas">
-    <div id="info_conquistas">
+  <div id="info_conquistas">
 
-       <div>
-          <p class="style-maintext"> Conquistas </p>
-       </div>
+    <div>
+      <p class="style-maintext"> Conquistas </p>
+    </div>
 
-    <div id="div_geral">
+    <div id="div_geral_progresso">
 
-       <!-- DIV DAS IMAGENS -->
-       <div id="div_img_conquistas">
-          <div class="background">
-            <img src="img/dashboard/ringbox.png">
-          </div>
-          <div class="background">
-            <img src="img/dashboard/cap.png">
-          </div>
-          <div class="background">
-            <img src="img/dashboard/seaker.png">
-          </div>
-          <div class="background">
-            <img src="img/dashboard/purse.png">
-          </div>
-       </div>
-
-       <!-- DIV DA BARRINHA DE PROGRESSO -->
-        <div id="div_progresso_conquistas">
-            <div class="div_titleBar_conquistas">
-                <p> Y2K </p>
-            </div>
-            <div class="div_titleBar_conquistas">
-                <p> Y2K </p>
-            </div>
-            <div class="div_titleBar_conquistas">
-                <p> Y2K </p>
-            </div>
-            <div class="div_titleBar_conquistas">
-                <p> Y2K </p>
-            </div>
+      <!-- DIV DAS IMAGENS -->
+      <div id="div_img_conquistas">
+        <div class="background">
+          <img src="img/dashboard/ringbox.png">
         </div>
+        <div class="background">
+          <img src="img/dashboard/cap.png">
+        </div>
+        <div class="background">
+          <img src="img/dashboard/seaker.png">
+        </div>
+        <div class="background">
+          <img src="img/dashboard/purse.png">
+        </div>
+      </div>
 
+      <!-- DIV DA BARRINHA DE PROGRESSO -->
+      <div id="div_progresso_conquistas">
+        <div class="div_titleBar_conquistas">
+          <!-- deu problema no css ent tive que estilizar
+              dentro da propria tag <p> (isso é temporario)
+              -->
+          <p style="font-weight: 600; color: #424245;">
+            Y2K <br>
+            <img src="img/dashboard/barra.png" class="img_barra_progresso">
+          </p>
+        </div>
+        <div class="div_titleBar_conquistas">
+          <p style="font-weight: 600; color: #424245;">
+            STREETWEAR
+            <img src="img/dashboard/barra.png" class="img_barra_progresso">
+          </p>
+        </div>
+        <div class="div_titleBar_conquistas">
+          <p style="font-weight: 600; color: #424245;">
+            UNIFORME ESCOLAR
+            <img src="img/dashboard/barra.png" class="img_barra_progresso">
+          </p>
+        </div>
+        <div class="div_titleBar_conquistas">
+          <p style="font-weight: 600; color: #424245;">
+            MCBLING
+            <img src="img/dashboard/barra.png" class="img_barra_progresso">
+          </p>
+
+        </div>
+      </div>
+
+    </div>  <!-- FECHA DIV_GERAL_PROGRESSO -->
+
+    <!-- BOTÃO CONSULTAR RANKING -->
+    <div id="div_btn_consulRanking">
+        
     </div>
 
 
-    </div>
-</div>
-
+  </div> <!-- FECHA INFO_CONQUISTAS -->
+</div>  <!-- FECHA CONTAINER_CONQUISTAS -->
 
 
 
 
 
 <?php
-    require_once("templates/footer.php");
+require_once("templates/footer.php");
 ?>
