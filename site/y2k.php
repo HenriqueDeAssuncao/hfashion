0a74c0ffae4017c8a196c456d6fdf387ed8de1df
jesbikers <jessica.almeida171@etec.sp.gov.br>
2023-06-27 11:47:26 -0300

    ...

--- site/y2k.php
+++ site/y2k.php
@@ -16,7 +16,7 @@ require_once "templates/header.php";
 
  <div class="container-primeiro-pag">
     <div id="img-primeiro-pag">
-       <img src="img/y2k/y2k_artigo.png" id="img-pag">
+       <img src="img/y2k/y2k_artigo.png" class="img-pag">
     </div>
 
   <div id="div-primeiro-pag">
@@ -26,12 +26,33 @@ require_once "templates/header.php";
       <br>
       <br>
       Essa estética é caracterizada por elementos futuristas, cyberpunk e kitsch, muitas vezes com uma mistura de tecnologia 
-          antiga e moderna. As cores costumam ser brilhantes e vibrantes, e a moda Y2K inclui roupas justas, tecidos brilhantes, calças de cintura 
-          baixa, tops cropped, tênis plataforma e acessórios como correntes e cintos largos.</p>
+      antiga e moderna. As cores costumam ser brilhantes e vibrantes, e a moda Y2K inclui roupas justas, tecidos brilhantes, calças de cintura 
+      baixa, tops cropped, tênis plataforma e acessórios como correntes e cintos largos.</p>
+
+  </div>
+  </div>
+
+  <div class="container-segundo-pag">
+    
+
+  <div id="div-segundo-pag">
+    <p class="txt-pag">A estética Y2K é uma tendência que remonta ao final dos anos 90 e ao início dos anos 2000. Y2K é uma abreviação de "Year 2000",
+      que se refere à época em que muitas pessoas temiam que o mundo sofresse um colapso devido a um suposto erro do "bug do milênio".
+      A estética Y2K é uma espécie de homenagem nostálgica a essa era.
+      <br>
+      <br>
+      Essa estética é caracterizada por elementos futuristas, cyberpunk e kitsch, muitas vezes com uma mistura de tecnologia 
+      antiga e moderna. As cores costumam ser brilhantes e vibrantes, e a moda Y2K inclui roupas justas, tecidos brilhantes, calças de cintura 
+      baixa, tops cropped, tênis plataforma e acessórios como correntes e cintos largos.</p>
 
   </div>
 
+  <div id="img-segundo-pag">
+       <img src="img/y2k/y2k_artigo.png" class="img-pag">
+    </div>
+
 </div>
+
 
 
 
