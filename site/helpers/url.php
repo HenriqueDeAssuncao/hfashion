b1feb3e4a794df12c2b510bc466407bda25fc473
HenriqueDeAssuncao <104686167+HenriqueDeAssuncao@users.noreply.github.com>
2023-09-09 16:23:51 -0300

    teste

--- site/helpers/url.php
+++ site/helpers/url.php
@@ -1,3 +1,8 @@
 <?php
+<<<<<<< Updated upstream
         session_start();
         $CURRENT_URL = "http://" . $_SERVER['SERVER_NAME'] .":8080". dirname($_SERVER['REQUEST_URI']. '?');
+=======
+session_start();
+$CURRENT_URL = "http://" . $_SERVER['SERVER_NAME'] . dirname($_SERVER['REQUEST_URI']. '?');
+>>>>>>> Stashed changes
