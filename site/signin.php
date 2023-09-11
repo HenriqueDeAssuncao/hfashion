ced8611269d191e2f8e652d515e259f9e02613c5
Etec <etec@etec.com>
2023-09-11 12:03:29 -0300

    telas de login e cadastro prontas

--- site/signin.php
+++ site/signin.php
@@ -4,52 +4,46 @@ require_once("templates/header_auth.php");
 
 <link rel="stylesheet" href="<?= $CURRENT_URL ?>/css/login.css">
 
-<a class="back" href="<?= $CURRENT_URL ?>/index.php"><img src="<?= $CURRENT_URL ?>/img/login/Back.svg"
-        style="height:30px" alt=""></a>
+<a href="<?= $CURRENT_URL ?>/index.php">
+    <img class="back" src="<?= $CURRENT_URL ?>/img/login/Back.svg" alt="">
+</a>
 
 <div style="display:inline-flexbox" class="Flex Full-height">
 
-    <div style="display:inline-flexbox" id="register-container">
-
-        <img src="<?= $CURRENT_URL ?>/img/login/titulo.svg" style="height:15px" alt="">
-        <p class="title">Conecte-se ao <br> HiFashion</p>
-
-        <div class="container-msg">
-            <?php
-            require_once("templates/message.php");
-            ?>
-        </div>
-
-
-        <form action="<?= $CURRENT_URL ?>/process/auth_process.php" method="POST">
-            <input type="hidden" name="type" value="login">
-            <div class="form-group">
-                <label class="email" for="nickname_email">E-mail ou Apelido:</label>
-                <input type="text" class="form-input" id="nickname_email" name="nickname_email" value="<?= $nickname_email ?>">
+        <div style="display:inline-flexbox" id="register-container">
+            <img src="<?= $CURRENT_URL ?>/img/login/titulo.svg" style="height:15px" alt="">
+            <p class="title">Conecte-se ao <br> HiFashion</p>
+            <div class="container-msg">
+                <?php
+                require_once("templates/message.php");
+                ?>
             </div>
-
-            <div class="form-group flex-2">
-                <label for="password">Senha:</label>
-                <input type="password" placeholder="8+ caracteres" class="form-input" id="password" name="password" value="<?= $password ?>">
-            </div>
-
-            <div class="form-container">
-                <input type="submit" class="btn-con" value="Conectar">
+            <form action="<?= $CURRENT_URL ?>/process/auth_process.php" method="POST">
+                <input type="hidden" name="type" value="login">
                 <div class="form-group">
-                    <p>Não tem uma conta?</p>
-                    <p><a href="<?= $CURRENT_URL ?>/signup.php">Faça seu cadastro</a></p>
+                    <label class="email" for="nickname_email">E-mail ou Apelido:</label>
+                    <input type="text" class="form-input" id="nickname_email" name="nickname_email" value="<?= $nickname_email ?>">
                 </div>
-            </div>
-        </form>
-    </div>
-
-    <div style="inline-block" class="visual Flex Full-height">
-        <div class="visual-content Flex Full-height ">
-            <img class="logo" src="<?= $CURRENT_URL ?>/img/login/logo.svg" style="width: 200px" alt="">
-            <img class="txt" src="<?= $CURRENT_URL ?>/img/login/texto.png" style="width: 290px" alt="">
-            <img class="foto" src="<?= $CURRENT_URL ?>/img/login/log1.png" style="width: 375px;" alt="">
+                <div class="form-group flex-2">
+                    <label for="password">Senha:</label>
+                    <input type="password" placeholder="8+ caracteres" class="form-input" id="password" name="password" value="<?= $password ?>">
+                </div>
+                <div class="form-container">
+                    <input type="submit" class="btn-con" value="Conectar">
+                    <div class="form-group">
+                        <p>Não tem uma conta?</p>
+                        <p><a href="<?= $CURRENT_URL ?>/signup.php">Faça seu cadastro</a></p>
+                    </div>
+                </div>
+            </form>
         </div>
-    </div>
+        <div style="inline-block" class="visual Flex Full-height">
+            <div class="visual-content Flex Full-height ">
+                <img class="logo" src="<?= $CURRENT_URL ?>/img/login/logo.svg" style="width: 200px" alt="">
+                <img class="txt" src="<?= $CURRENT_URL ?>/img/login/texto.png" style="width: 290px" alt="">
+                <img class="foto" src="<?= $CURRENT_URL ?>/img/login/log1.png" style="width: 375px;" alt="">
+            </div>
+        </div>
 
 </div>
 
