f406e94748af13fc3b7a127cf5ac6a669501c348
HenriqueDeAssuncao <104686167+HenriqueDeAssuncao@users.noreply.github.com>
2023-10-26 13:09:33 -0300

    Arrumando edit_profile

--- src/edit_profile.php
+++ src/edit_profile.php
@@ -1,10 +1,37 @@
 <?php
-require_once("templates/header.php");
+require_once "templates/header.php";
 ?>
 
 <link rel="stylesheet" href="<?= $CURRENT_URL ?>/css/edit_profile.css">
 
 <!-- PARTE SUPERIOR -->
+
+<div class="Container">
+    <form action="<?=$CURRENT_URL?>/process/user_process.php" method="POST" enctype="multipart/form-data" method="POST">
+        <div class="profile">
+            <label for="image">Foto de perfil</label>
+            <div class="profile-img" style="width: 100px; height: 100px; background-image: url('<?=$CURRENT_URL?>/<?=$image?>')" alt="Foto de Perfil"></div>
+            <?php if(!empty($userAvatars) && count($userAvatars)):?>
+                <div class="user-avatars">
+                <?php foreach($userAvatars as $UserAvatar):?>
+                    <div class="user-avatar"> 
+                        <label for="avatar-path">
+                            <img src="<?=$CURRENT_URL?>/<?=$UserAvatar->getAvatarPath()?>" alt="Imagem avatar">
+                        </label>
+                        <input type="radio" name="avatar-path" value="<?=$UserAvatar->getAvatarPath()?>">
+                    </div>
+                <?php endforeach;?>
+        </div>
+        <div class="password">
+
+        </div>
+        <input class="btn-submit" type="submit" value="Salvar mudanças">
+    </form>
+</div>
+
+
+
+
 <div class="pi">
     <form>
         <!-- FOTOS PARA O PERFIL -->
