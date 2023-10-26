73129029851e64ab33b49ea4855fce614b95e3bd
HenriqueDeAssuncao <104686167+HenriqueDeAssuncao@users.noreply.github.com>
2023-10-26 11:02:37 -0300

    coloquei o código da jack

--- src/edit_profile.php
+++ src/edit_profile.php
@@ -1,79 +1,54 @@
 <?php
-require_once "templates/header.php";
-
-require_once "models/User.php";
-require_once "dao/UserDAO.php";
-require_once "dao/UserAvatarDAO.php";
-
-$user = new User();
-$userDao = new UserDAO($conn, $CURRENT_URL);
-$userData = $userDao->verifyToken(true);
-
-$UserAvatarDao = new UserAvatarDAO($conn, $CURRENT_URL);
-$userAvatars = $UserAvatarDao->findAvatars($userData->getId());
-
+require_once("templates/header.php");
 ?>
 
 <link rel="stylesheet" href="<?= $CURRENT_URL ?>/css/edit_profile.css">
 
-<style>
-    .user-avatar img {
-        width: 100px;
-    }
-</style>
-
-<div id="edit-container">
-    <form action="<?= $CURRENT_URL ?>/process/user_process.php" method="POST" enctype="multipart/form-data">
-        <input type="hidden" name="type" value="update">
-        <h2>Altere seus dados no formulário abaixo:</h2>
-        <h2>Perfil:</h2>
-        <div class="form-group">
-            <label>Foto:</label>
-            <div class="profile-img" style="width: 100px; height: 100px; background-image: url('<?=$CURRENT_URL?>/<?=$image?>')" alt="Foto de Perfil"></div>
-            <?php if(!empty($userAvatars) && count($userAvatars)):?>
-                <div class="user-avatars">
-                <?php foreach($userAvatars as $UserAvatar):?>
-                    <div class="user-avatar"> 
-                        <label for="avatar-path">
-                            <img src="<?=$CURRENT_URL?>/<?=$UserAvatar->getAvatarPath()?>" alt="Imagem avatar">
-                        </label>
-                        <input type="radio" name="avatar-path" value="<?=$UserAvatar->getAvatarPath()?>">
-                    </div>
-                <?php endforeach;?>
+<!-- PARTE SUPERIOR -->
+<div class="pi">
+    <form>
+        <!-- FOTOS PARA O PERFIL -->
+        <div class="bttxt2">
+            <h3 class="txt2">Foto de perfil</h3>
+        </div>
+        <div class="bt2">
+            <div class="icon">
+                <label for="selecionar-arquivo">
+                    <img src="img/edit_profile/icon1.png" alt="Imagem 1">
+                    <img src="img/edit_profile/icon2.png" alt="Imagem 2">
+                    <img src="img/edit_profile/icon3.png" alt="Imagem 3">
+                </label>
+                <input id="selecionar-arquivo" type="file" style="display: none;">
+                <img src="img/edit_profile/icon4.png" alt="Imagem 4">
             </div>
-            <?php endif;?>
-            <input type="submit" value="Salvar Imagem">
         </div>
-        <div class="form-group">
-            <label for="bio">Altere sua bio:</label>
-            <input type="bio" class="form-input" name="bio" value="<?= $userData->getBio() ?>">
-        </div>
-        <div class="form-group">
-            <label for="nickname">Digite o seu novo apelido:</label>
-            <input type="nickname" class="form-input" name="nickname" placeholder="Digite o novo apelido">
-        </div>
-        <div class="form-container">
-            <input type="submit" class="Button" value="Salvar">
+        <div class="legend">
+            <p>Responda os quizzes para desbloquear as fotos para seu perfil</p>
         </div>
     </form>
+</div>
 
-    <form action="<?= $CURRENT_URL ?>/process/user_process.php" method="POST">
-        <input type="hidden" name="type" value="changepassword">
-        <h2>Senha:</h2>
-        <p>Digite a sua nova senha e confirme para alterá-la:</p>
+<!-- FORMULÁRIO -->
+<div class="boxtext">
+    <form>
         <div class="form-group">
-            <label for="password">Senha:</label>
-            <input type="password" class="form-input" name="password" placeholder="Digite a nova senha">
+            <label class="email" for="email">E-mail:</label>
+            <input type="email" class="form-input" id="email" name="email" required>
         </div>
+
         <div class="form-group">
-            <label for="confirmpassword">Confirmação de senha:</label>
-            <input type="password" class="form-input" name="confirmpassword" placeholder="Confirme a senha">
+            <label class="bio" for="text">Biografia</label>
+            <textarea id="my-textarea" required></textarea>
         </div>
-        <div class="form-container">
-            <input type="submit" class="Button" value="Alterar">
-        <div>
+
+        <div class="form-group">
+            <label class="nome" for="nickname">Nome:</label>
+            <input type="text" class="form-input" id="nickname" name="nickname" required>
+        </div>
+        <div class="bt">
+            <button class="submit-btn" type="submit"><span>Salvar mudanças</span></button>
+        </div>
     </form>
-
 </div>
 
 <?php
