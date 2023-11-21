68ee3d320025d365ca56a4954f618b45f6793e24
Scorpiorisiing <132522078+Scorpiorisiing@users.noreply.github.com>
2023-11-21 00:46:33 -0300

    inicio da tela de criar quiz

--- src/adm/quiz.php
+++ src/adm/quiz.php
@@ -1,29 +1,31 @@
 <?php
-require_once "templates/header.php";
+require_once "../templates/header.php";
 ?>
 
 <?php
 require_once __DIR__ . "/../templates/message.php";
 ?>
+</head>
 
 <div class="Container">
     <h1>Criar quiz</h1>
     <form action="process/adm_quiz_process.php" method="POST" enctype="multipart/form-data">
         <div class="form-group">
             <label for="quiz-name">Nome:</label>
-            <input type="text" name="quiz-name" placeholder="Digite o nome do quiz">
+            <input type="text" name="quiz-name" placeholder="Digite o nome do quiz" class="type">
         </div>
         <div class="form-group">
             <label for="quiz-description">Descrição</label>
-            <input type="text" name="quiz-description" placeholder="Crie uma descrção para o quiz">
+            <input type="text" name="quiz-description" placeholder="Crie uma descrção para o quiz" class="type">
         </div>
         <div class="form-group">
-            <label for="question-weight">Peso das questões:</label>
+            <label for="question-weight" class="type">Peso das questões:</label>
             <select name="question-weight">
                 <option value="1">1</option>
                 <option value="2">2</option>
                 <option value="3">3</option>
             </select>
+            <hr>
         </div>
         <div class="form-group">
             <div class="form-section">
@@ -32,24 +34,25 @@ require_once __DIR__ . "/../templates/message.php";
             </div>
             <div class="form-section">
                 <label for="emblem-name">Nome:</label>
-                <input type="text" name="emblem-name">
+                <input type="text" name="emblem-name" class="type">
             </div>
         </div>
         <div class="form-group">
             <label for="quiz-icon">ícone:</label>
             <input type="file" name="icon">
+            <hr>
         </div>
 
         <div id="avatars" class="form-group">
             <div class="form-section">
-                <label for="avatars">Insira os avatares:</label>
-                <input type="file" name="first-avatar">
-                <input type="file" name="second-avatar">
+                <label for="avatars" class="custom-file-upload">Insira os avatares:</label>
+                <input type="file" name="first-avatar" id="file-upload">
+                <input type="file" name="second-avatar" id="file-upload">
             </div>
-            <div class="form-section">
+            <div class="form-section names">
                 <label for="avatars-names">Nomes:</label>
-                <input type="text" name="avatars-names[]">
-                <input type="text" name="avatars-names[]">
+                <input type="text" name="avatars-names[]" class="names">
+                <input type="text" name="avatars-names[]" class="names">
             </div>
         </div>
         <input type="submit" class="Button" value="Criar Quiz!">
@@ -59,6 +62,7 @@ require_once __DIR__ . "/../templates/message.php";
 <link rel="stylesheet" href="css/index.css">
 <link rel="stylesheet" href="<?= $CURRENT_URL ?>/../css/global.css">
 <link rel="stylesheet" href="<?= $CURRENT_URL ?>/../css/message.css">
+<link rel="stylesheet" href="<?= $CURRENT_URL ?>/../css/create_quiz.css">
 <script src="<?= $CURRENT_URL ?>/../script/message.js"></script>
 
 <?php
